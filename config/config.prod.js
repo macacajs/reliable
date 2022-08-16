c058c1c347582caa891d1bc57f2f66bc63677c94
达峰的夏天 <xudafeng@126.com>
2022-08-16 11:37:27 +0800

    chore: enhance config (#88)

--- config/config.prod.js
+++ config/config.prod.js
@@ -2,3 +2,11 @@
 
 const dbConfig = require('../database/config');
 exports.sequelize = dbConfig.production;
+
+// eslint-disable-next-line no-unused-vars
+module.exports = appInfo => {
+  const config = exports = {};
+
+  // inject-extra-config
+  return config;
+};

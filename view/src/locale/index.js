f875d0c14b46a32e158b56f634a16b7eb71947fc
xudafeng <xudafeng@126.com>
2022-12-11 23:14:30 +0800

    fix: en switcher

--- view/src/locale/index.js
+++ view/src/locale/index.js
@@ -3,7 +3,8 @@ import en from './en-US';
 
 window.__i18n = window.__i18n || locale({
   en,
-  getLanguage() {
-    return window.localStorage.RELIABLE_LANGUAGE || window.navigator.language;
+  useEn() {
+    const language = window.localStorage.RELIABLE_LANGUAGE || window.navigator.language;
+    return language.startsWith('en');
   },
 });
